4951a3d7c88942c540e774a86209f2d103e177d0
GajicMladen <mladengajic00@gmail.com>
2023-01-14 19:05:47 +0100

    simple-controler

---
+++ nwt-kts-api/src/main/java/com/example/nwtktsapi/controller/DriveController.java
@@ -0,0 +1,34 @@
+package com.example.nwtktsapi.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+@RequestMapping(value = "api/drive")
+public class DriveController {
+
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @PostMapping(value = "/startDrive")
+    public ResponseEntity<String> startDrive(){
+
+        List<String> values = Arrays.asList("1","2","5");
+        String url = "http://localhost:3000/receive-values";
+        HttpEntity<List<String>> request = new HttpEntity<>(values);
+        restTemplate.postForObject(url, request, Void.class);
+
+
+        return ResponseEntity.ok("OK");
+    }
+}
