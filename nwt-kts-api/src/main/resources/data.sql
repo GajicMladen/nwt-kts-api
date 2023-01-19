d50af92f8ff2614280b76dc81ace4c4cf4e47ce6
Jovan Tomic <jtomic1@gmail.com>
2023-01-20 00:57:41 +0100

    Edit profile - upload slika

--- nwt-kts-api/src/main/resources/data.sql
+++ nwt-kts-api/src/main/resources/data.sql
@@ -4,29 +4,29 @@ insert into vehicle_price(type, price) values(2, 350);
 insert into vehicle_price(type, price) values(3, 300);
 insert into vehicle_price(type, price) values(4, 400);
 
-insert into users( dtype , email ,password ,name ,lastname ,phone  )
-values('Driver', 'm','m','Mladen','Gajic' , '0644281080');
+insert into users( dtype , email ,password ,name ,lastname ,phone, profile_photo, town)
+values('Driver', 'm','m','Mladen','Gajic' , '0644281080', 'https://i.ibb.co/VCfhmKQ/image.jpg', 'Novi Sad');
 
-insert into users( dtype , email ,password ,name ,lastname ,phone  )
-values('Driver', 'j','j','Jovan','Tomic' , '06965466');
+insert into users( dtype , email ,password ,name ,lastname ,phone, profile_photo, town  )
+values('Driver', 'j','j','Jovan','Tomic' , '06965466', 'https://i.ibb.co/VCfhmKQ/image.jpg', 'Novi Sad');
 
-insert into users( dtype , email ,password ,name ,lastname ,phone  )
-values('Driver', 'd','d','Djosa','Jovanovic' , '0666595632');
+insert into users( dtype , email ,password ,name ,lastname ,phone, profile_photo, town  )
+values('Driver', 'd','d','Djosa','Jovanovic' , '0666595632', 'https://i.ibb.co/VCfhmKQ/image.jpg', 'Novi Sad');
 
-insert into users( dtype , email ,password ,name ,lastname ,phone ,active )
-values('Client', 'c','$2a$10$XeS1WZloSVVq2Z2dJd3L7ePADJy51sWu/oLqcy.Qcmppr6VcUtcr6','Marko','Markovic' , '0644281080',true);
+insert into users( dtype , email ,password ,name ,lastname ,phone ,active, profile_photo, town )
+values('Client', 'c','$2a$10$XeS1WZloSVVq2Z2dJd3L7ePADJy51sWu/oLqcy.Qcmppr6VcUtcr6','Marko','Markovic' , '0644281080',true, 'https://i.ibb.co/VCfhmKQ/image.jpg', 'Novi Sad');
 
-insert into users( dtype , email ,password ,name ,lastname ,phone,active  )
-values('Client', 'a','$2a$10$XeS1WZloSVVq2Z2dJd3L7ePADJy51sWu/oLqcy.Qcmppr6VcUtcr6','Nikola','Nikolic' , '0644281080',true);
+insert into users( dtype , email ,password ,name ,lastname ,phone,active, profile_photo, town  )
+values('Client', 'a','$2a$10$XeS1WZloSVVq2Z2dJd3L7ePADJy51sWu/oLqcy.Qcmppr6VcUtcr6','Nikola','Nikolic' , '0644281080',true, 'https://i.ibb.co/VCfhmKQ/image.jpg', 'Novi Sad');
 
-insert into users( dtype , email ,password ,name ,lastname ,phone ,active )
-values('Client', 'b','$2a$10$XeS1WZloSVVq2Z2dJd3L7ePADJy51sWu/oLqcy.Qcmppr6VcUtcr6','Lazar','Lazarovic' , '0644281080',true);
+insert into users( dtype , email ,password ,name ,lastname ,phone ,active, profile_photo, town )
+values('Client', 'b','$2a$10$XeS1WZloSVVq2Z2dJd3L7ePADJy51sWu/oLqcy.Qcmppr6VcUtcr6','Lazar','Lazarovic' , '0644281080',true, 'https://i.ibb.co/VCfhmKQ/image.jpg', 'Novi Sad');
 
-insert into users( dtype , email ,password ,name ,lastname ,phone, active  )
-values('Client', 'p3r5kul45@gmail.com','lozinka1','Jovan','Tomic' , '0644281080', true);
+insert into users( dtype , email ,password ,name ,lastname ,phone, active, profile_photo, town  )
+values('Client', 'p3r5kul45@gmail.com','lozinka1','Jovan','Tomic' , '0644281080', true, 'https://i.ibb.co/VCfhmKQ/image.jpg', 'Novi Sad');
 
-insert into users( dtype , email ,password ,name ,lastname ,phone, active  )
-values('Client', 'jtomic1@gmail.com','$2a$10$0W7w6ZXLQPXonylfuYH80uuLPeM/v52yAs37OHvhbrkqQi7Ki8b4q','Jovan','Tomic' , '0644281080', true);
+insert into users( dtype , email ,password ,name ,lastname ,phone, active, profile_photo, town  )
+values('Client', 'jtomic1@gmail.com','$2a$10$0W7w6ZXLQPXonylfuYH80uuLPeM/v52yAs37OHvhbrkqQi7Ki8b4q','Jovan','Tomic' , '0644281080', true, 'https://i.ibb.co/VCfhmKQ/image.jpg', 'Novi Sad');
 
 
 INSERT INTO MESSAGE("CONTENT","ADMIN_MESSAGE","TIME_STAMP","USER_ID")
