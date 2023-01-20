e989fb0d0781f8c61d1010cc6d7b05d6911da36f
Jovan Tomic <jtomic1@gmail.com>
2023-01-20 20:52:52 +0100

    Edit profila

--- nwt-kts-api/src/main/java/com/example/nwtktsapi/controller/UserController.java
+++ nwt-kts-api/src/main/java/com/example/nwtktsapi/controller/UserController.java
@@ -1,25 +1,31 @@
 package com.example.nwtktsapi.controller;
 
 
-import com.example.nwtktsapi.dto.UserDTO;
-import com.example.nwtktsapi.model.User;
-import com.example.nwtktsapi.service.TransformToDTOService;
-import com.example.nwtktsapi.service.UserService;
+import java.security.Principal;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.example.nwtktsapi.dto.ChangeUserDataDTO;
+import com.example.nwtktsapi.dto.ResetPasswordDTO;
+import com.example.nwtktsapi.dto.UserDTO;
+import com.example.nwtktsapi.model.DriverChangeRequest;
 import com.example.nwtktsapi.model.User;
+import com.example.nwtktsapi.service.DriverChangeService;
 import com.example.nwtktsapi.service.TransformToDTOService;
 import com.example.nwtktsapi.service.UserService;
+import com.example.nwtktsapi.utils.ErrMsg;
+import com.google.gson.Gson;
 
-@Controller
+@RestController
 @RequestMapping(value = "api/users/")
 public class UserController {
 
@@ -28,6 +34,9 @@ public class UserController {
 
     @Autowired
     private TransformToDTOService transformToDTOService;
+    
+    @Autowired
+    private DriverChangeService driverChangeService;
 
     @GetMapping(value = "all")
     public ResponseEntity<List<Object>> getAllUsers(){
@@ -43,5 +52,45 @@ public class UserController {
         UserDTO userDTO = new UserDTO(user);
 
         return new ResponseEntity<>(userDTO,HttpStatus.OK);
+    }
+    
+    @PostMapping(value= "resetPassword")
+    public ResponseEntity<?> resetPassword(Principal user, @RequestBody ResetPasswordDTO resetDTO){
+		Gson gson = new Gson();
+		
+		if(!resetDTO.validate())
+			return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY)
+					.body(gson.toJson(new ErrMsg("Uneti podaci su neispravni!")));
+		
+		User u = userService.findByEmail(user.getName());
+		userService.setEncryptedPassword(u, resetDTO.getNewPassword());
+		userService.save(u);
+		
+		return new ResponseEntity<>(HttpStatus.OK);
+    }
+    
+    @PostMapping(value = "changeUserData")
+    public ResponseEntity<?> changeUserData(Principal user, @RequestBody ChangeUserDataDTO dataDTO){
+    	Gson gson = new Gson();
+    	User u = userService.findByEmail(user.getName());
+    	dataDTO.casify();
+    	
+    	if(!dataDTO.validate())
+			return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY)
+					.body(gson.toJson(new ErrMsg("Uneti podaci su neispravni!")));
+    	
+    	if (!u.hasRole("ROLE_DRIVER")) {
+    		u.setName(dataDTO.getName());
+    		u.setLastName(dataDTO.getLastName());
+    		u.setPhone(dataDTO.getPhone());
+    		u.setTown(dataDTO.getTown());
+    		u.setProfilePhoto(dataDTO.getPhoto());
+    		userService.save(u);
+    	} else {
+    		DriverChangeRequest request = new DriverChangeRequest(dataDTO, u);
+    		driverChangeService.createDriverChangeRequest(request);
+    	}
+    	
+    	return new ResponseEntity<>(dataDTO, HttpStatus.OK);
     }
 }
--- nwt-kts-api/src/main/resources/data.sql
+++ nwt-kts-api/src/main/resources/data.sql
@@ -56,5 +56,5 @@ INSERT INTO USER_ROLE(user_id, role_id) VALUES (4, 1);
 INSERT INTO USER_ROLE(user_id, role_id) VALUES (5, 1);
 INSERT INTO USER_ROLE(user_id, role_id) VALUES (6, 2);
 INSERT INTO USER_ROLE(user_id, role_id) VALUES (7, 1);
-INSERT INTO USER_ROLE(user_id, role_id) VALUES (8, 1);
+INSERT INTO USER_ROLE(user_id, role_id) VALUES (8, 3);
 INSERT INTO USER_ROLE(user_id, role_id) VALUES (5, 2);
