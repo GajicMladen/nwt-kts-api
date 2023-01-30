029b0b72397e0093ab37ec9636ae8b05e5271c9e
Jovan Tomic <jtomic1@gmail.com>
2023-01-30 17:30:09 +0100

    Minor fix

--- nwt-kts-api/src/main/java/com/example/nwtktsapi/dto/FavouriteRouteDTO.java
+++ nwt-kts-api/src/main/java/com/example/nwtktsapi/dto/FavouriteRouteDTO.java
@@ -1,17 +1,26 @@
 package com.example.nwtktsapi.dto;
 
+import com.example.nwtktsapi.model.Coordinate;
 import com.example.nwtktsapi.model.Fare;
 
 public class FavouriteRouteDTO {
 
 	private String routeName;
 	private Long clientId;
+	private String stops;
 	
 	public FavouriteRouteDTO() {};
 	
 	public FavouriteRouteDTO(Long clientId, Fare fare) {
 		this.clientId = clientId;
 		this.routeName = fare.getStops().get(0).getName() + " - " + fare.getStops().get(fare.getStops().size() - 1).getName();
+	
+		this.stops = "";
+		for (Coordinate cord: fare.getStops()) {
+            this.stops = this.stops.concat(cord.getName()+","+
+                    String.valueOf(cord.getLatitude())+","+
+                    String.valueOf(cord.getLongitude())+";");
+        }
 	}
 	
 	public String getRouteName() {
@@ -26,7 +35,13 @@ public class FavouriteRouteDTO {
 	public void setClientId(Long clientId) {
 		this.clientId = clientId;
 	}
-	
-	
-	
+
+	public String getStops() {
+		return stops;
+	}
+
+	public void setStops(String stops) {
+		this.stops = stops;
+	}
+
 }
